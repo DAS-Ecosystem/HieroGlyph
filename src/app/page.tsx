dfd9a952cee10ca44790d0cedc96279c60d9cfb1
DAS-Ecosystem <indranil.das@krishnayekare.com>
2025-08-30 06:06:39 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-import { Metadata } from "next";
 import dynamic from "next/dynamic";
+import { Metadata } from "next";
+
 import { meta } from "@/lib/utils";
 
-// Use dynamic import to prevent SSR for AuthForm
+// Dynamically import AuthForm to prevent SSR errors
 const AuthForm = dynamic(() => import("@/components/AuthForm"), { ssr: false });
 
 export const metadata: Metadata = meta;
 
 export default function Home(): React.JSX.Element {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#f0f2f5] to-[#efeae2] py-12 dark:from-[#222e35] dark:to-[#0b141a] sm:px-6 lg:px-8">
-      <AuthForm />
-    </div>
-  );
+	return (
+		<div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#f0f2f5] to-[#efeae2] py-12 dark:from-[#222e35] dark:to-[#0b141a] sm:px-6 lg:px-8">
+			<AuthForm />
+		</div>
+	);
 }
